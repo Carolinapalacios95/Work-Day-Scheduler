084da4fd6ec47fde2d381fd46e57ebbdd3f62658
Carolina <carolina.palacios1995@gmail.com>
2023-02-27 10:39:52 -0500

    modified html ids, added jumbotron to css & if statement to js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-// ***THIS FIRST LINE WRAPS**** all code that interacts with the DOM in a call to jQuery to ensure that
+// ***FIRST LINE WRAPS**** all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// List Global Variables
-// Referencing day.js
-
-// made a var that references the whole task (time and textarea)
-var containerEl = document.querySelector('.container');
-
-
-
 $(function () {
+  // made a var that references day.js as shorthand
   var today = dayjs();
-  // Displays the current date and time in p tag with ID of "currentDay"
+
+  // Displays the current date and time in the header p tag with ID of "currentDay"
   $('#currentDay').text(today.format('MMMM D, YYYY h:mm a'));
 
   // added event listener for saveBtn class within each time div
@@ -25,24 +19,53 @@ $(function () {
     //sets items to store in local storage
     localStorage.setItem(timeKey, textInput)
   })
+  
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //loads any saved data from LocalStorage for each hour from in html
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
   
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  function hourTracker() {
+    //gets current hour
+    var currentHour = today.hour(); 
+    
+    // loops over each div with the time block ID
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    
+
+      // if the time ID is before the current hour, add the past class from CSS
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+     } // if the time ID is equal to the current hour, remove the past and future classes & add the present class
+      else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+      } // If the time ID is greater than the current time, remove the past and present classes & add the future class
+      else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+  //calls the hour tracker function to execute
+  hourTracker();
+
+  // Use setTimeout to update the time every minute (1000ms * 60s)
+  setTimeout(function () {
+    // clears the current URL
+    location = ''; // location references the current URL
+  }, 1000 * 60);
 });
+
